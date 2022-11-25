5991fce205f6a1099387e50b0a94b071d4085a3c
Rando Õispuu <rando@veebisepad.ee>
2022-11-25 10:18:55 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Visit Saaremaa Jõulukaart
+
+Steps to get it running:
+
+In your terminal:
+
+1. Install dependencies:
+
+```bash
+    npm install
+```
+
+2. Run the dev server
+
+```bash
+    npm run dev
+```
+
+3. Open the the link shown in your browser of choice
+
+# More info:
+
+## Vue 3 + Vite
+
+This template should help get you started developing with Vue 3 in Vite. The template uses Vue 3 `<script setup>` SFCs, check out the [script setup docs](https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup) to learn more.
+
+### Recommended IDE Setup
+
+- [VS Code](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar)
+
+```
+
+```
---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { PauseIcon, PlayIcon, StopIcon } from "@heroicons/vue/24/solid";
+import { ref } from "vue";
+import Lego from "./assets/lego.json";
+
+const anim = ref(null);
+
+const play = () => anim.value.play();
+const stop = () => anim.value.stop();
+const pause = () => anim.value.pause();
+</script>
+<template>
+  <div class="flex flex-col gap-2 items-center">
+    <Vue3Lottie ref="anim" :animationData="Lego" :height="400" :width="400" />
+    <div class="flex gap-2">
+      <button
+        class="bg-green-400 px-4 py-2 rounded shadow-md active:scale-90 text-white"
+        @click="play"
+      >
+        <PlayIcon class="h-4 w-4" />
+      </button>
+      <button
+        class="bg-yellow-400 px-4 py-2 rounded shadow-md active:scale-90 text-white"
+        @click="pause"
+      >
+        <PauseIcon class="h-4 w-4" />
+      </button>
+      <button
+        class="bg-red-400 px-4 py-2 rounded shadow-md active:scale-90 text-white"
+        @click="stop"
+      >
+        <StopIcon class="h-4 w-4" />
+      </button>
+    </div>
+  </div>
+</template>
